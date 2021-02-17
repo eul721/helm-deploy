be6e6e2f968438252ea8c6baf5d2ffca0d952360
Ryan Andonian <ryan.andonian@take2games.com>
2021-02-17 11:12:24 -0800

    adds: github workflows adds: jest config adds: single dummy test file

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// Place test-specific environment properies and values here
+const TestEnv = {
+  NODE_ENVIRONMENT: 'test',
+};
+
+Object.keys(TestEnv).forEach(key => {
+  process.env[key] = TestEnv[key];
+});
+
+module.exports = {
+  modulePathIgnorePatterns: ['dist'],
+};
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe('src/index', () => {
+  it('should establish test scaffolding', () => {
+    expect(true).toBeTruthy();
+  });
+});
