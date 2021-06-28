b6efbc2ae79d6e288b016a2e2a9df079454c1921
Jack Chao <jack.chao@take2games.com>
2021-06-28 15:14:40 -0700

    fix credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,12 @@ CHART_LOCATION=$5
 VALUE_FILES=$6
 VALUES=$7
 
-KUBETOKEN=$(aws eks get-token --cluster-name $CLUSTER --region us-east-1 | jq -c -r '.status.token')
-CLUSTER_ENDPOINT=$(aws eks describe-cluster --name $CLUSTER --region us-east-1 | jq -c -r '.cluster.endpoint')
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+export AWS_REGION=us-east-1
+
+KUBETOKEN=$(aws eks get-token --cluster-name $CLUSTER  | jq -c -r '.status.token')
+CLUSTER_ENDPOINT=$(aws eks describe-cluster --name $CLUSTER  | jq -c -r '.cluster.endpoint')
 
 echo $KUBETOKEN
 echo $CLUSTER_ENDPOINT
